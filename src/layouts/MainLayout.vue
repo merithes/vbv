f1f48524150a7f0a197e8c4394cb26b492a09020
= <=victor@boivin.dev>
2021-10-03 19:22:50 +0200

    Full redesign. New colors, new fonts, new SVG animation on the homepage, the hamburger menu button icon has been replaced with an animated SVG more in tune with the new theme

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,32 +1,153 @@
 <template>
-  <q-layout view="hHh Lpr lFf">
+  <q-layout view="hHh Lpr lFf" id="pageLayout">
     <transition
       appear
       enter-active-class="animated fadeInDown"
       leave-active-class="animated fadeOutUp"
     >
-      <q-toolbar class="fixed-top z-top bg-dark text-secondary text-bold shadow-3 full-width">
+      <q-toolbar class="fixed-top bg-dark z-top text-secondary text-bold shadow-3 full-width">
         <div class="container">
-          <q-btn icon="menu" flat square @click="showMenu = !showMenu" class="no-border-radius vbv-neon"/>
+          <q-btn
+            flat
+            square
+            @click="showMenu = !showMenu"
+            @mouseenter="toggleEyeHover(true)"
+            @mouseleave="toggleEyeHover(false)"
+            class="no-border-radius vbv-neon"
+            id="mainMenuBtn"
+          >
+            <template v-slot:default>
+              <svg id="eyeIcon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 66 43" width="2em">
+                <g id="eyeIrisGroup">
+                  <path
+                    id="eyeIris"
+                    d="M 43 28 a 14.1 14.1 0 0 0 0 -16 m -20 0 a 14.1 14.1 0 0 0 0 16"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    vector-effect="non-scaling-stroke"
+                  ></path>
+                  <animate
+                    id="eyeIrisToggleIn"
+                    ref="eyeIrisToggleIn"
+                    href="#eyeIris"
+                    attributeName="d"
+                    from="M 23 30 a 45 45 0 0 0 20 0 a 14.1 14.1 0 0 0 0 -20 a 45 45 0 0 0 -20 0 a 14.1 14.1 0 0 0 0 20"
+                    to="M 23 30 a 14.1 14.1 0 0 0 20 0 a 14.1 14.1 0 0 0 0 -20 a 14.1 14.1 0 0 0 -20 0 a 14.1 14.1 0 0 0 0 20"
+                    dur="0.13s"
+                    fill="freeze"
+                  ></animate>
+                  <animate
+                    v-if="$q.screen.gt.sm"
+                    id="eyeIrisME"
+                    ref="eyeIrisME"
+                    href="#eyeIris"
+                    attributeName="d"
+                    from="M 43 28 a 14.1 14.1 0 0 0 0 -16 m -20 0 a 14.1 14.1 0 0 0 0 16"
+                    to="M 43 30 a 14.1 14.1 0 0 0 0 -20 m -20 0 a 14.1 14.1 0 0 0 0 20"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                  <animate
+                    v-if="$q.screen.gt.sm"
+                    id="eyeIrisML"
+                    ref="eyeIrisML"
+                    href="#eyeIris"
+                    attributeName="d"
+                    from="M 43 30 a 14.1 14.1 0 0 0 0 -20 m -20 0 a 14.1 14.1 0 0 0 0 20"
+                    to="M 43 28 a 14.1 14.1 0 0 0 0 -16 m -20 0 a 14.1 14.1 0 0 0 0 16"
+                    dur="0.2s"
+                    fill="freeze"
+                    begin="0s"
+                  ></animate>
+                  <animate
+                    id="eyeIrisToggleOut"
+                    ref="eyeIrisToggleOut"
+                    href="#eyeIris"
+                    attributeName="d"
+                    from="M 23 30 a 14.1 14.1 0 0 0 20 0 a 14.1 14.1 0 0 0 0 -20 a 14.1 14.1 0 0 0 -20 0 a 14.1 14.1 0 0 0 0 20"
+                    to="M 23 30 a 45 45 0 0 0 20 0 a 14.1 14.1 0 0 0 0 -20 a 45 45 0 0 0 -20 0 a 14.1 14.1 0 0 0 0 20"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                </g>
+                <g id="eyeOutlineGroup">
+                  <path
+                    id="eyeOutline"
+                    d="M 3 20 a 53 53 0 0 0 60 0 a 53 53 0 0 0 -60 0"
+                    fill="transparent"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    vector-effect="non-scaling-stroke"
+                  />
+                  <animate
+                    href="#eyeOutline"
+                    id="eyeOutlineToggleIn"
+                    ref="eyeOutlineToggleIn"
+                    attributeName="d"
+                    from="M 3 20 a 45 45 0 0 0 60 0 a 45 45 0 0 0 -60 0"
+                    to="M 3 20 a 35 35 0 0 0 60 0 a 35 35 0 0 0 -60 0"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                  <animate
+                    v-if="$q.screen.gt.sm"
+                    id="eyeOutlineME"
+                    ref="eyeOutlineME"
+                    href="#eyeOutline"
+                    attributeName="d"
+                    from="M 3 20 a 53 53 0 0 0 60 0 a 53 53 0 0 0 -60 0"
+                    to="M 3 20 a 45 45 0 0 0 60 0 a 45 45 0 0 0 -60 0"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                  <animate
+                    v-if="$q.screen.gt.sm"
+                    id="eyeOutlineML"
+                    ref="eyeOutlineML"
+                    href="#eyeOutline"
+                    attributeName="d"
+                    from="M 3 20 a 45 45 0 0 0 60 0 a 45 45 0 0 0 -60 0"
+                    to="M 3 20 a 53 53 0 0 0 60 0 a 53 53 0 0 0 -60 0"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                  <animate
+                    id="eyeOutlineToggleOut"
+                    ref="eyeOutlineToggleOut"
+                    href="#eyeOutline"
+                    attributeName="d"
+                    from="M 3 20 a 35 35 0 0 0 60 0 a 35 35 0 0 0 -60 0"
+                    to="M 3 20 a 45 45 0 0 0 60 0 a 45 45 0 0 0 -60 0"
+                    dur="0.2s"
+                    fill="freeze"
+                  ></animate>
+                </g>
+              </svg>
+            </template>
+          </q-btn>
         </div>
       </q-toolbar>
     </transition>
     <q-page-container class="vbv-body-content">
       <router-view />
     </q-page-container>
-    <q-drawer v-model="showMenu" elevated>
-      <div class="row q-px-md q-py-xl full-height">
+    <q-drawer v-model="showMenu" elevated id="menuDrawer">
+      <div class="row q-px-md q-py-xl full-height cinzel vbv-bg-gold">
         <div class="col-xs-12 q-pt-lg relative-position">
           <template v-for="(link, index) in menuLinks">
             <div :key="index">
               <q-separator v-if="index > 0" spaced/>
               <q-btn
-              class="full-width no-border-radius q-pl-md t-normal-ease vbv-menu-link"
-              :class="{'vbv-menu-current-link': link.location === currentPath}"
-              align="left"
-              flat
-              color="primary"
-              v-bind="link"
+                class="full-width no-border-radius q-pl-md t-normal-ease vbv-menu-link text-weight-bolder"
+                :class="{'vbv-menu-current-link': link.location === currentPath}"
+                align="left"
+                flat
+                v-bind="link"
               />
             </div>
           </template>
@@ -81,7 +202,8 @@
       return {
         showHeader: true,
         showMenu: false,
-        menuLinks: links
+        menuLinks: links,
+        hoverEye: false
       }
     },
     computed: {
@@ -90,9 +212,32 @@
       },
       currentPath() {
         return this.$route.path
-      },
+      }
+    },
+    watch: {
+      showMenu() {
+        if (this.showMenu) {
+          this.$refs.eyeOutlineToggleIn.beginElement()
+          this.$refs.eyeIrisToggleIn.beginElement()
+        } else {
+          this.$refs.eyeOutlineToggleOut.beginElement()
+          this.$refs.eyeIrisToggleOut.beginElement()
+        }
+      }
     },
     methods: {
+      toggleEyeHover(hovered) {
+        this.hoverEye = hovered
+        if (!this.showMenu && hovered) {
+          console.log(hovered)
+          this.$refs.eyeOutlineME.beginElement()
+          this.$refs.eyeIrisME.beginElement()
+        } else if (!this.showMenu && !hovered) {
+          console.log(hovered)
+          this.$refs.eyeOutlineML.beginElement()
+          this.$refs.eyeIrisML.beginElement()
+        }
+      }
     },
     created() {
       this.$q.loadingBar.setDefaults({
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,31 +1,230 @@
 <template>
-  <q-page class="row justify-center" @scroll="setScrollView()">
+  <q-page class="row justify-center vbv-home" @scroll="setScrollView()">
     <div id="vbvHomeHead" class="col-xs-12 relative-position" style="height: 100vh">
       <div
-        class="flex items-center column full-width full-height"
+        class="flex items-center column full-width full-height relative-position overflow-hidden justify-center"
         :class="{
-          'q-px-xl justify-center': screenData.gt.xs,
+          'q-px-xl': screenData.gt.xs,
           'q-px-md vbv-home-responsive-container': screenData.lt.sm
           }"
       >
-        <div class="vbv-home-head-container block coda text-weight-bolder q-my-none text-grey-5 text-center">
-          <h1 class="vbv-home-head-content-wrapper" :class="{'text-h2' : screenData.gt.sm && screenData.lt.md, 'text-h3' : screenData.lt.sm}">
+        <svg viewBox="0 0 2000 2000" class="vbv-home-clock absolute-center text-white full-width full-height">
+          <defs>
+            <mask id="centerCut">
+              <rect width="100%" height="100%" fill="white"/>
+              <circle
+                r="300"
+                cx="50%"
+                cy="50%"
+                fill="black"
+                stroke="none"
+              />
+            </mask>
+            <mask id="smallHole">
+              <rect width="100%" height="100%" fill="white"/>
+              <circle r="25" fill="black">
+                <animateMotion
+                  keyPoints=".375;.375"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+              <circle r="25" fill="black">
+                <animateMotion
+                  keyPoints=".875;.875"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+            </mask>
+            <mask id="backgroundCumulatedHoles">
+              <rect width="100%" height="100%" fill="white"/>
+              <circle
+                r="300"
+                cx="50%"
+                cy="50%"
+                fill="black"
+                stroke="none"
+              />
+              <circle r="25" fill="black">
+                <animateMotion
+                  keyPoints=".375;.375"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+              <circle r="25" fill="black">
+                <animateMotion
+                  keyPoints=".875;.875"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+            </mask>
+        		<filter id="rough">
+        			<feTurbulence type="fractalNoise" baseFrequency="1" numOctaves="100" result="turbulence"/>
+        			<feDisplacementMap in2="turbulence" in="SourceGraphic" scale="1" xChannelSelector="R" yChannelSelector="G"/>
+        		</filter>
+          </defs>
+          <!-- poor performance on mobile -->
+          <g :filter="screenData.gt.sm ? 'url(#rough)' : ''">
+            <g id="rays" mask="url(#backgroundCumulatedHoles)" fill="#ffd927">
+              <polygon
+                points="-100,-100 0,-690 100,-100 100,100 0,460 -100,100"
+                transform="translate(1000,1000)"
+              >
+                <animateTransform
+                  id="secRayAnim"
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 0 0"
+                  to="360 0 0"
+                  dur="60s"
+                  repeatCount="indefinite"
+                  additive="sum"
+                  :begin="((dateTime.getSeconds()) - 60) + 's'"
+                  animation-play-state="paused"
+                />
+              </polygon>
+              <polygon
+                points="-100,-100 0,-880 100,-100 540,0 100,100 -100,100 -990Z,0"
+                transform="translate(1000,1000)"
+              >
+                <animateTransform
+                  id="minRayAnim"
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 0 0"
+                  to="360 0 0"
+                  dur="3600s"
+                  repeatCount="indefinite"
+                  additive="sum"
+                  begin="indefinite"
+                  :begin="((dateTime.getSeconds() + (60 * (dateTime.getMinutes()))) - 3600) + 's'"
+                  v-once
+                />
+              </polygon>
+              <polygon
+                points="-100,-100 0,-600 100,-100 100,100 0,400 -100,100 -500,0"
+                fill="lihtgray"
+                transform="translate(1000,1000)"
+              >
+                <animateTransform
+                  id="hourRayAnim"
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 0 0"
+                  to="360 0 0"
+                  dur="43200s"
+                  repeatCount="indefinite"
+                  additive="sum"
+                  :begin="((dateTime.getSeconds() + (60 * (dateTime.getMinutes() + (60 * dateTime.getHours())))) - 86400) / 2 + 's'"
+                  v-once
+                />
+              </polygon>
+              <animate attributeName="opacity" dur="10s" from="0" to="1"/>
+            </g>
+            <g id="outerCircles" stroke="#fddb00">
+              <circle
+                cx="50%"
+                cy="50%"
+                r="400"
+                fill="none"
+                stroke-width="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                vector-effect="non-scaling-stroke"
+              />
+              <circle
+                cx="50%"
+                cy="50%"
+                r="500"
+                fill="none"
+                stroke-width="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                vector-effect="non-scaling-stroke"
+              />
+            </g>
+            <g id="innerCircle">
+              <circle
+                cx="1000"
+                cy="50%"
+                r="300"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                vector-effect="non-scaling-stroke"
+                mask="url(#smallHole)"
+              />
+            </g>
+            <g id="orbitalCircles">
+              <circle r="25" fill="none" stroke="currentColor" stroke-width="10" vector-effect="non-scaling-stroke">
+                <animateMotion
+                  keyPoints=".375;.375"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+              <circle r="25" fill="none" stroke="currentColor" stroke-width="10" vector-effect="non-scaling-stroke">
+                <animateMotion
+                  keyPoints=".875;.875"
+                  fill="freeze"
+                  keyTimes="0;1"
+                  calcMode="linear"
+                  path="M 700 1000 a 300 300 0 0 1 600 0 a 300 300 0 0 1 -600 0"
+                ></animateMotion>
+              </circle>
+            </g>
+            <g id="orbitalDashes">
+              <path
+                mask="url(#centerCut)"
+                d="
+                M 1000 1000 l -50 -320
+                M 1000 1000 l -190 -280
+                M 1000 1000 l -280 -190
+                M 1000 1000 l -320 -50
+                M 1000 1000 l 50 320
+                M 1000 1000 l 190 280
+                M 1000 1000 l 280 190
+                M 1000 1000 l 320 50
+                "
+                fill="none"
+                stroke="currentColor"
+                stroke-width="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                vector-effect="non-scaling-stroke"
+              ></path>
+          </g>
+          </g>
+        </svg>
+        <div class="vbv-home-head-container block text-weight-bolder q-my-none text-dark text-center">
+          <h1 class="vbv-home-head-content-wrapper q-mb-md cinzel" :class="{'text-h2' : screenData.gt.sm && screenData.lt.md, 'text-h3 q-mt-none' : screenData.lt.sm}">
             <span class="vbv-home-head-word">Victor </span><span class="vbv-home-head-word">Boivin</span>
           </h1>
-          <div class="vbv-home-head-content-glitch" :class="{'text-h2' : screenData.gt.sm && screenData.lt.md, 'text-h3' : screenData.lt.sm}">
-            <span class="vbv-home-head-word">Victor </span><span class="vbv-home-head-word">Boivin</span>
-          </div>
-          <div class="vbv-home-head-underline"></div>
         </div>
         <div
-          class="vbv-home-subtitle coda q-mt-lg text-center small-line"
-          :class="{
-            'text-h6' : screenData.name === 'sm',
-            'text-h5' : screenData.gt.sm,
-            'text-subtitle1' : screenData.lt.sm
+        class="vbv-home-subtitle text-weight-bold q-mt-lg text-center small-line fenix relative-position"
+        :class="{
+          'text-h5' : screenData.name === 'sm',
+          'text-h4' : screenData.gt.sm,
+          'text-h6' : screenData.lt.sm
           }"
         >
-          Hello,&nbsp;my&nbsp;name&nbsp;is&nbsp;Victor. I&nbsp;am&nbsp;a&nbsp;web&nbsp;developer&nbsp;&amp;&nbsp;programmer at&nbsp;Youdemus&nbsp;Paris.
+          Hello. My&nbsp;name&nbsp;is&nbsp;Victor. I&nbsp;am&nbsp;a&nbsp;web&nbsp;developer&nbsp;&amp;&nbsp;programmer at&nbsp;Youdemus&nbsp;Paris.
         </div>
       </div>
     </div>
@@ -38,8 +237,8 @@
     name: 'Home',
     data() {
       return {
-        profilePic: 'https://picsum.photos/600',//'~assets/profilePic.png',
-        viewHeight: '0px'
+        viewHeight: '0px',
+				dateTime: new Date()
       }
     },
     computed: {
@@ -53,19 +252,6 @@
       }
     },
     created() {
-      // let checkScrollView = throttle ((oldScroll, scrollY) => {
-      //   let vbvHomeContent = document.getElementById("vbvHomeContent")
-      //   let vbvHomeHead = document.getElementById("vbvHomeHead")
-      //   if (scrollY > oldScroll && vbvHomeHead.offsetTop + vbvHomeHead.offsetHeight > scrollY) {
-      //     vbvHomeContent.scrollIntoView()
-      //   } else if (scrollY < oldScroll && vbvHomeHead.offsetHeight > scrollY + 50) {
-      //     vbvHomeHead.scrollIntoView()
-      //   }
-      // }, 100)
-      // window.onscroll = function(e) {
-      //   checkScrollView(typeof this.oldScroll !== 'undefined' ? this.oldScroll : 0, this.scrollY)
-      //   this.oldScroll = this.scrollY
-      // }
     }
   }
 </script>
