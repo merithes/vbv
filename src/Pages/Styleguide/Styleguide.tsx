ff834505302941b261e7c33c1879ac94eeaaac3a
Victor Boivin <victor.boivin@nextmobiles.com>
2023-07-17 16:57:31 +0200

    added button labels ; more responsive

--- src/Pages/Styleguide/Styleguide.tsx
+++ src/Pages/Styleguide/Styleguide.tsx
@@ -7,35 +7,35 @@ const Styleguide = () => {
           <div className="col-12">
             <h2>Colors</h2>
           </div>
-          <div className="col-xs-6 col-md-3 col-lg-2">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2">
             <div>primary</div>
             <div className="row gap-none">
               <div className="col-6 bg-primary pt-xl"></div>
               <div className="col-6 bg-primary-dimmed"></div>
             </div>
           </div>
-          <div className="col-xs-6 col-md-3 col-lg-2">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2">
             <div>primary-light</div>
             <div className="row gap-none">
               <div className="col-6 bg-primary-light pt-xl"></div>
               <div className="col-6 bg-primary-light-dimmed"></div>
             </div>
           </div>
-          <div className="col-xs-6 col-md-3 col-lg-2">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2">
             <div>secondary</div>
             <div className="row gap-none">
               <div className="col-6 bg-secondary pt-xl"></div>
               <div className="col-6 bg-secondary-dimmed"></div>
             </div>
           </div>
-          <div className="col-xs-6 col-md-3 col-lg-2">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2">
             <div>accent</div>
             <div className="row gap-none">
               <div className="col-6 bg-accent pt-xl"></div>
               <div className="col-6 bg-accent-dimmed"></div>
             </div>
           </div>
-          <div className="col-xs-6 col-md-3 col-lg-2">
+          <div className="col-xs-12 col-sm-6 col-md-3 col-lg-2">
             <div>highlight</div>
             <div className="row gap-none">
               <div className="col-6 bg-highlight pt-xl"></div>
@@ -47,19 +47,23 @@ const Styleguide = () => {
           <div className="col-12">
             <h2>Buttons</h2>
           </div>
-          <div className="col-xs-12 col-md-6 row">
+          <div className="col-xs-12 col-sm-6 row">
             <div className="col-12">
+              <div>default color</div>
               <Button label="Default" />
             </div>
             <div className="col-12">
+              <div>default color</div>
               <Button large label="Large" />
             </div>
           </div>
-          <div className="col-xs-12 col-md-6 row">
+          <div className="col-xs-12 col-sm-6 row">
             <div className="col-12">
+              <div>alt color</div>
               <Button color="accent" label="Accent" />
             </div>
             <div className="col-12">
+              <div>alt color</div>
               <Button color="highlight" large label="Highlight" />
             </div>
           </div>
@@ -70,17 +74,21 @@ const Styleguide = () => {
           </div>
           <div className="col-xs-12 col-md-6 row">
             <div className="col-12">
+              <div>default color</div>
               <Button disabled label="Default" />
             </div>
             <div className="col-12">
+              <div>default color</div>
               <Button disabled large label="Large" />
             </div>
           </div>
           <div className="col-xs-12 col-md-6 row">
             <div className="col-12">
+              <div>alt color</div>
               <Button disabled color="accent" label="Accent" />
             </div>
             <div className="col-12">
+              <div>alt color</div>
               <Button disabled color="highlight" large label="Highlight" />
             </div>
           </div>
