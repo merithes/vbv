b49a4dd00975e67349a3484497cdb72cf15e42ef
Victor Boivin <victor.boivin@nextmobiles.com>
2023-07-19 22:31:35 +0200

    removing overcomplicated scroll indicator; tweaking formatting rules

--- src/Layouts/DefaultLayout/DefaultLayout.tsx
+++ src/Layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,39 +1,13 @@
 import { Menu } from 'src/Components/Menu/Menu'
 import { useLocation, useOutlet } from 'react-router-dom'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
-import {useRef, useEffect, useState} from 'react'
+import { useRef } from 'react'
 import './DefaultLayout.scss'
-import classNames from 'classnames'
 
 const Layout = () => {
   const location = useLocation(),
-        pageRef = useRef<HTMLDivElement | null>(null),
-        containerRef = useRef<HTMLDivElement | null>(null),
-        outletRef = useRef<HTMLDivElement | null>(null),
-        currentOutlet = useOutlet(),
-        [overflow, setOverflow] = useState(false),
-        [fullyScrolled, setFullyScrolled] = useState(false)
-  const checkHeight = () => {
-    let outletHeight: number = typeof outletRef?.current?.clientHeight !== 'undefined' ? outletRef?.current?.clientHeight : 0,
-        pageHeight: number = typeof containerRef?.current?.clientHeight !== 'undefined' ? containerRef?.current?.clientHeight : 0,
-        pagePadding: number = (pageRef?.current !== null ? parseInt(getComputedStyle(pageRef?.current!).paddingTop) : 0),
-        containerPadding: number = (containerRef?.current !== null ? parseInt(getComputedStyle(containerRef?.current!).paddingTop) : 0)
-
-    console.log((outletHeight > (pageHeight - pagePadding - containerPadding)))
-    setOverflow(outletHeight > (pageHeight - pagePadding - containerPadding))
-  }
-  const managePageScroll = (e: Event) => {
-    const {target} = e
-    if (target !== null) {
-      console.log(target, target.scrollHeight, target.clientHeight, target.scrollHeight - target.clientHeight, target.scrollTop)
-      if (target.scrollHeight - target.clientHeight === target.scrollTop) {
-        setFullyScrolled(true);
-      } else {
-        setFullyScrolled(false);
-      }
-    }
-  }
-  useEffect(checkHeight, []);
+    outletRef = useRef<HTMLDivElement | null>(null),
+    currentOutlet = useOutlet()
   return (
     <div className="layoutContainer row">
       <div className="menuContainer col-2 pa-md bg-grey-dimmed bd-lightgrey border-r">
@@ -44,17 +18,22 @@ const Layout = () => {
           ]}
         />
       </div>
-      <div className="page col-10 pa-md full-vh" ref={pageRef}>
-        <div className="pageContainer pa-md bg-grey-dimmed full-height" ref={containerRef}>
-          <div className={classNames("pageContentWrapper position-relative", {overflowing: overflow})} onScroll={managePageScroll}>
+      <div className="page col-10 pa-md full-vh">
+        <div className="pageContainer pa-md bg-grey-dimmed full-height">
+          <div
+            className="pageContentWrapper position-relative"
+            tabIndex={0}
+            role="region"
+            aria-label="page content"
+          >
             <SwitchTransition>
               <CSSTransition
                 key={location.pathname}
                 nodeRef={outletRef}
                 timeout={100}
                 classNames="page"
-                onEnter={checkHeight}
-                unmountOnExit>
+                unmountOnExit
+              >
                 <div ref={outletRef}>{currentOutlet}</div>
               </CSSTransition>
             </SwitchTransition>
